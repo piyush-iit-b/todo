55f0066537c816c17082e1584ad0896f9c38c5d5
piyush-iit-b <onlyforiestudy@gmail.com>
2026-09-03 14:25:44 +0530

    Update app.js

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ const router = require("./model2/routes");
 
 app.use(cors({
   origin: [
-    "https://your-frontend-app.vercel.app", // Replace with your actual Vercel domain
+    "https://tranquil-banoffee-8445cf.netlify.app", // Replace with your actual Vercel domain
     "http://localhost:5173",                 // For local testing
     "http://localhost:5000" 
   ],
